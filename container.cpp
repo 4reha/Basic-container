055004a73ae93393a01fe1e2539148abe69358c8
4reha <ael-hadd@student.1337.ma>
2022-10-04 18:35:27 +0100

    isolated container

--- container.cpp
+++ container.cpp
@@ -1,19 +1,12 @@
-/**
- * @ Author: AbderraHman El Haddad Asofi (4reha)
- * @ Create Time: 2022-10-02 16:23:29
- * @ Modified by: AbderraHman El Haddad Asofi (4reha)
- * @ Modified time: 2022-10-02 17:40:36
- * @ Description:
- */
-
 #include <iostream>
+#include <unistd.h>
 #include <sched.h>
 #include <sys/types.h>
-#include <unistd.h>
+#include <sys/mount.h>
 #include <sys/wait.h>
 
 char*	stack_memory()	{
-	int stack_size = 65536;
+	int stack_size = 50000;
 	char *stack = new char[stack_size];
 
 	if (!stack)	{
@@ -24,12 +17,23 @@ char*	stack_memory()	{
 }
 
 int jail(void	*args)	{
+
 	std::cout << "jail called" << std::endl;
+	char **cmd = (char **)args;
+	chroot("./fake_root");
+	chdir("/");
+	mount("proc", "/proc", "proc", 0, 0);
+	if (!fork())
+		execve(cmd[0], cmd, NULL);
+	wait(NULL);
+	umount("/proc");
 	return (0);
 }
 
-int	main(void)	{
+int	main(int ac, char** av)	{
 	std::cout << "Hello World!" << std::endl;
-	clone(jail, stack_memory(), SIGCHLD, 0);
+	clone(jail, stack_memory(), CLONE_NEWUTS | CLONE_NEWPID | SIGCHLD, &av[1]);
+	wait(NULL);
+
     return (0);
 }
